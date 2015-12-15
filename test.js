8c04bd8d1e79936240128647f8a2e94ec31c7ae1
Patrick Shaughnessy <pshaughn86@gmail.com>
2015-12-15 12:23:29 -0800

    gets all swapi people

--- test.js
+++ test.js
@@ -2,21 +2,27 @@
 
 var request = require('request');
 
-function getPerson() {
+function getPerson(i) {
   var person;
 
   return new Promise(function(resolve, reject) {
-    request('http://swapi.co/api/people/1', function(error, response, body) {
-      person = body;
+    request(`http://swapi.co/api/people/${i}`, function(error, response, body) {
+      person = JSON.parse(body);
       resolve(person);
     });
   })
 }
 
 async function main() {
-  var person = await getPerson();
-  console.log(person);
+  let people = [];
+
+  for (let i = 1; i <= 87; i++) {
+    let person = await getPerson(i);
+    people.push({number: i, name: person.name});
+    console.log('person', i, person.name);
+  }
+  console.log('people', people);
 }
 
 main();
-console.log('this should be first');
+console.log('fetching star wars people');
