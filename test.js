a204472f81d90d5e51479e56dbc208dd1aa5cec4
Patrick Shaughnessy <pshaughn86@gmail.com>
2015-12-15 13:09:54 -0800

    recursive function for fetching all people

--- test.js
+++ test.js
@@ -2,26 +2,62 @@
 
 var request = require('request');
 
-function getPerson(i) {
-  var person;
+// function getPerson(i) {
+//   var person;
+//
+//   return new Promise(function(resolve, reject) {
+//     request(`http://swapi.co/api/people/${i}`, function(error, response, body) {
+//       person = JSON.parse(body);
+//       resolve(person);
+//     });
+//   })
+// }
+//
+// async function main() {
+//   let people = [];
+//
+//   for (let i = 1; i <= 87; i++) {
+//     let person = await getPerson(i);
+//     people.push({number: i, name: person.name});
+//     console.log('person', i, person.name);
+//   }
+//   console.log('people', people);
+// }
+//
+// main();
+// console.log('fetching star wars people');
+
+function getPeoplePage(page) {
+  var people;
+  var next;
+
+  var url = page || "http://swapi.co/api/people/";
 
   return new Promise(function(resolve, reject) {
-    request(`http://swapi.co/api/people/${i}`, function(error, response, body) {
-      person = JSON.parse(body);
-      resolve(person);
+    request(url, function(error, response, body) {
+      var data = JSON.parse(body);
+      people = data.results.map(person => person.name);
+      next = data.next
+      resolve({people: people, next: next});
     });
   })
 }
 
 async function main() {
-  let people = [];
+  var allPeople = [];
 
-  for (let i = 1; i <= 87; i++) {
-    let person = await getPerson(i);
-    people.push({number: i, name: person.name});
-    console.log('person', i, person.name);
+  async function getPeople(page){
+    var {people, next} = await getPeoplePage(page)
+    allPeople = allPeople.concat(people);
+    if (next){
+      getPeople(next);
+    } else {
+      // no more pages - return all people;
+      console.log(allPeople);
+    }
   }
-  console.log('people', people);
+  
+  getPeople();
 }
 
 main();
